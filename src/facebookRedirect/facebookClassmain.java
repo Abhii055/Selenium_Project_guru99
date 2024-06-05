9aa1e5977d74e31666b96cb88ee5cce37a0061ee
abhinav <abhinavdwivedi0807@gmail.com>
2024-06-06 00:30:38 +0530

    facebook_testing

---
+++ src/facebookRedirect/facebookClassmain.java
@@ -0,0 +1,12 @@
+package facebookRedirect;
+
+public class facebookClassmain {
+
+	public static void main(String[] args) {
+		facebookClass fb= new facebookClass();
+		fb.invokeBrowser();
+		fb.CAFR();
+		fb.CA();
+	}
+
+}
--- src/firstSeleniumScript/WorkingWithChrome.java
+++ src/firstSeleniumScript/WorkingWithChrome.java
@@ -1,53 +1,53 @@
-//package firstSeleniumScript;
-//
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//public class WorkingWithChrome {
-//
-//	ChromeDriver driver;
-//
-//	public void invokeBrowser() {
-//		String url = "https://test.qatechhub.com";
-//
-//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhin\\eclipse-workspace\\libs\\chromedriver.exe");
-//
-//		driver = new ChromeDriver();
-//
-//		driver.manage().window().maximize();
-//		driver.get(url);
-//	}
-//
-//	public void printTitle() {
-//		String expectedTitle = "QA Automation Tools Trainings and Tutorials | QA Tech Hub";
-//		String actualTitle = driver.getTitle();
-//		if (expectedTitle.equals(actualTitle)) {
-//			System.out.print("PASS");
-//		} else {
-//			System.out.print("FAIL");
-//		}
-//
-//	}
-//	public void navigateCommands() {
-//		driver.get("https://www.facebook.com");
-//		driver.navigate().back();
-//		String urlofCurrentWeb= driver.getCurrentUrl();
-//		System.out.println(urlofCurrentWeb);
-//		driver.navigate().forward();
-//		driver.navigate().refresh();
-//	}
-//
-//	public void closeBrowser() {
-//		driver.quit(); // to close all windows
-//	}
-//
-//	public static void main(String[] args) {
-//		WorkingWithChrome wc = new WorkingWithChrome();
-//		wc.invokeBrowser();
-//		wc.printTitle();
-//		wc.navigateCommands();
-//		wc.closeBrowser();
-//	}
-//}
+package firstSeleniumScript;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WorkingWithChrome {
+
+	ChromeDriver driver;
+
+	public void invokeBrowser() {
+		String url = "https://test.qatechhub.com";
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhin\\eclipse-workspace\\libs\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get(url);
+	}
+
+	public void printTitle() {
+		String expectedTitle = "QA Automation Tools Trainings and Tutorials | QA Tech Hub";
+		String actualTitle = driver.getTitle();
+		if (expectedTitle.equals(actualTitle)) {
+			System.out.print("PASS");
+		} else {
+			System.out.print("FAIL");
+		}
+
+	}
+	public void navigateCommands() {
+		driver.get("https://www.facebook.com");
+		driver.navigate().back();
+		String urlofCurrentWeb= driver.getCurrentUrl();
+		System.out.println(urlofCurrentWeb);
+		driver.navigate().forward();
+		driver.navigate().refresh();
+	}
+
+	public void closeBrowser() {
+		driver.quit(); // to close all windows
+	}
+
+	public static void main(String[] args) {
+		WorkingWithChrome wc = new WorkingWithChrome();
+		wc.invokeBrowser();
+		wc.printTitle();
+		wc.navigateCommands();
+		wc.closeBrowser();
+	}
+}
 
 //User ID :	mngr575227
 //Password :	EjazEde
